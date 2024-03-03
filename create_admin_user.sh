0249923f9aa9501abc6d237170ff8be98aa42731
official-repos <108777242+official-repos@users.noreply.github.com>
2024-03-03 16:36:36 +0700

    Update create_admin_user.sh

--- create_admin_user.sh
+++ create_admin_user.sh
@@ -17,6 +17,8 @@ fi
 
 # Create the user
 useradd -m "$username" --shell "/bin/bash"
+# Add the user to the sudo group
+sudo usermod -aG sudo "$username"
 
 # Prompt for password
 read -sp "Enter password: " password

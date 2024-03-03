713b6a799ed4a4ea543758ce4f6bf0f86e9ff4d9
Official Repos <official-repos@gmail.com>
2024-03-03 13:08:54 +0700

    added create-vhost.sh

--- create-vhost.sh
+++ create-vhost.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
-git pull
 
-echo -n "Enter fqdn: "
+#git pull
+
+echo -n "Enter FQDN: "
 read FQDN
-echo ${FQDN}
 
-#sudo cp ./files/vhost-template.conf /etc/apache2/sites-available/${FQDN}
+# Check if input is empty
+if [ -z "${FQDN}" ]; then
+    echo "Input is empty. Exiting."
+    exit 1
+fi
+
+echo "Processing virtual host..."
+
+VDIRCFG=/etc/apache2/sites-available/${FQDN}.conf
+sudo cp ./files/vhost-template.conf ${VDIRCFG}
+sudo sed -i -e "s/___DOMAIN___/${FQDN}/g" ${VDIRCFG}
+
+VDIR=/var/www/${FQDN}
+# Check if the directory exists
+if [ ! -d "$VDIR" ]; then
+    # If it doesn't exist, create it
+    sudo mkdir -p "$VDIR/html"
+    echo "<?php phpinfo(); ?>" | sudo tee ${VDIR}/html/test-phpinfo.php
+    echo "Directory created: $VDIR"
+else
+    echo "Directory already exists: $VDIR"
+    exit 1
+fi
+
+ls -l /var/www/
+
+sudo a2ensite ${FQDN}
+sudo systemctl restart apache2
+
+

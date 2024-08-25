e52c36d9f4ce7c6a6ec9bc645ada13118c7d036b
Official Repos <official-repos@gmail.com>
2024-08-25 09:58:48 +0700

    added setdns.sh

---
+++ misc/setdns.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install resolvconf 
+sudo systemctl enable --now resolvconf.service
+
+sudo nano /etc/resolvconf/resolv.conf.d/head
+# Use these nameservers
+# nameserver 8.8.8.8 
+# nameserver 8.8.4.4
+
